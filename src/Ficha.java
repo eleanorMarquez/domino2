ac0f33d70e80c17ff7734f69ed8c426c61086c20
EleanorMarquez <ruthestrellahernandez@gmail.com>
2019-02-21 21:09:46 -0500

    first commit

---
+++ src/Ficha.java
@@ -0,0 +1,59 @@
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Ficha {
+    
+    private int pinta1, pinta2;
+    //constructor por defecto
+    public Ficha(){
+    }
+    
+    //constructor sobrecargado
+    public Ficha(int v1, int v2){
+    
+        this.pinta1=v1;
+        this.pinta2=v2;
+    
+    }
+    
+
+
+    public int getPinta1() {
+        return pinta1;
+    }
+
+    public void setPinta1(int pinta1) {
+        this.pinta1 = pinta1;
+    }
+
+    public int getPinta2() {
+        return pinta2;
+    }
+
+    public void setPinta2(int pinta2) {
+        this.pinta2 = pinta2;
+    }
+
+    public void rotarFicha(){
+        
+        int aux=this.pinta1;
+        this.pinta1=this.pinta2;
+        this.pinta2=aux;
+    }
+    
+    public int sumarFicha(){
+    
+        int aux=this.pinta1+this.pinta2;
+        return aux;
+    }
+    
+    public void intercambioFicha(Ficha x, Ficha y){
+        
+        Ficha aux;
+        aux=x;
+        x=y;
+        y=aux;  
+}
+}
---
+++ src/Main.java
@@ -0,0 +1,63 @@
+
+import java.util.ArrayList;
+
+
+public class Main{
+
+
+public static void main(String[] args){
+
+
+        Ficha f1=new Ficha(0,0);
+        Ficha f2=new Ficha (2,1);
+        
+        
+        ArrayList<Ficha> dominoes = new ArrayList<Ficha>();
+/*00*/		      dominoes.add(new Ficha(0, 0));
+/*01*/                dominoes.add(new Ficha(0, 1));
+/*02*/                dominoes.add(new Ficha(0, 2));
+/*03*/                dominoes.add(new Ficha(0, 3));
+/*04*/                dominoes.add(new Ficha(0, 4));
+/*05*/                dominoes.add(new Ficha(0, 5));
+/*06*/                dominoes.add(new Ficha(0, 6));
+/*07*/                dominoes.add(new Ficha(1, 1));
+/*08*/                dominoes.add(new Ficha(1, 2));
+/*09*/                dominoes.add(new Ficha(1, 3));
+/*10*/                dominoes.add(new Ficha(1, 4));
+/*11*/                dominoes.add(new Ficha(1, 5));
+/*12*/                dominoes.add(new Ficha(1, 6));
+/*13*/                dominoes.add(new Ficha(2, 2));
+/*14*/                dominoes.add(new Ficha(2, 3));
+/*15*/                dominoes.add(new Ficha(2, 4));
+/*16*/                dominoes.add(new Ficha(2, 5));
+/*17*/                dominoes.add(new Ficha(2, 6));
+/*18*/                dominoes.add(new Ficha(3, 3));
+/*19*/                dominoes.add(new Ficha(3, 4));
+/*20*/                dominoes.add(new Ficha(3, 5));
+/*21*/                dominoes.add(new Ficha(3, 6));
+/*22*/                dominoes.add(new Ficha(4, 4));
+/*23*/                dominoes.add(new Ficha(4, 5));
+/*24*/                dominoes.add(new Ficha(4, 6));
+/*25*/                dominoes.add(new Ficha(5, 5));
+/*26*/                dominoes.add(new Ficha(5, 6));                
+/*27*/                dominoes.add(new Ficha(6, 6));
+                
+                
+        
+        //metodo rotar
+       //7f1.rotarFicha();
+        //f2.rotarFicha();
+        //llamar metodo de intercamiar 2 fichas
+        //f1.intercambioFicha(f2, f2);
+        //System.out.println(f1.getPinta1()+":"+f1.getPinta2());
+        //System.out.println(f2.getPinta1()+":"+f2.getPinta2());
+        //metodo sumar ficha
+        /* f1.sumarFicha();
+        f2.sumarFicha();
+        
+        System.out.println(f1.getPinta1()+f1.getPinta2());
+        System.out.println(f2.getPinta1()+f2.getPinta2());
+        */
+
+}
+}
