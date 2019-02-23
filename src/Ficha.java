86afbe1c8c0a8cbd0e171ac8373ddc10647cd45d
EleanorMarquez <ruthestrellahernandez@gmail.com>
2019-02-23 00:33:39 -0500

    testeando ficha

--- src/Ficha.java
+++ src/Ficha.java
@@ -1,24 +1,16 @@
-
-/**
- *
- * @author LENOVO
- */
-public class Ficha {
-    
-    private int pinta1, pinta2;
-    //constructor por defecto
-    public Ficha(){
+public class ficha {
+    //atributos
+    private  int pinta1,pinta2;
+    //constructor por default
+    public ficha(){
     }
-    
     //constructor sobrecargado
-    public Ficha(int v1, int v2){
-    
-        this.pinta1=v1;
-        this.pinta2=v2;
-    
+    public ficha(int x, int y){
+       this.pinta1=x;
+       this.pinta2=y;
     }
     
-
+    //metodos get y set
 
     public int getPinta1() {
         return pinta1;
@@ -35,25 +27,32 @@ public class Ficha {
     public void setPinta2(int pinta2) {
         this.pinta2 = pinta2;
     }
-
-    public void rotarFicha(){
-        
+    
+    //metodo para rotar ficha
+    public void rotar(){
         int aux=this.pinta1;
         this.pinta1=this.pinta2;
         this.pinta2=aux;
+        
     }
     
-    public int sumarFicha(){
-    
-        int aux=this.pinta1+this.pinta2;
+    //metodo para sumar points
+    public int sumar(){
+        int aux= this.pinta1+this.pinta2;   
         return aux;
     }
     
-    public void intercambioFicha(Ficha x, Ficha y){
+    //metodo intercambiar
+    public void intercambio(ficha n){
+        int aux= this.pinta1;
+        int temp=this.pinta2;
+     
+        this.pinta1=n.pinta1;
+        this.pinta2=n.pinta2;
+        n.pinta1=aux;
+        n.pinta2=temp;
         
-        Ficha aux;
-        aux=x;
-        x=y;
-        y=aux;  
-}
+    }
+    
+
 }
---
+++ src/jugador.java
@@ -0,0 +1,21 @@
+  import java.util.ArrayList;
+  
+public class jugador {
+    
+private String nombre;
+private ArrayList<ficha> mano = new ArrayList<>();
+
+public jugador (){
+}
+
+public jugador (String nom, ArrayList man){
+ this.nombre=nom;
+ this.mano=man;
+}
+
+/*public ArrayList repatir(ArrayList dom){
+   
+    
+    
+}*/
+}
